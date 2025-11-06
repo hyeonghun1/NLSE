de0f246cca6ded91bd031836f30ca1cca3de06fd
Hyeonghun Kim <hyk049@ucsd.edu>
2025-11-06 14:58:00 -0800

    add Simul_SDE_guide.m

--- Simul_SDE_guide.m
+++ Simul_SDE_guide.m
@@ -97,5 +97,48 @@ itoerr = abs(ito - 0.5*(W(end)^2 - T))
 straterr = abs(strat - 0.5*W(end)^2)
 
 
+%% EM Euler-Maruyama method on linear SDE
+%
+% SDE is dX = lambda*X dt + mu*X dW, X(0) = Xzero,
+% where lambda = 2, mu = 1 and Xzero = 1.
+%
+% Discretized Brownian path over [0,1] has dt = 2^(-8).
+% Euler-Maruyama uses timestep R*dt.
+randn('state', 100)
+lambda = 2;
+mu = 1;
+Xzero = 1; % problem parameters
 
+T = 1;
+N = 2^8;
+dt = 1/N;
+
+dW = sqrt(dt)*randn(1,N); % Brownian increments
+W = cumsum(dW);           % discretized Brownian path
+
+% Exact solution
+Xtrue = Xzero * exp((lambda-0.5*mu^2) * ([dt:dt:T]) + mu*W);
+
+figure;
+plot([0:dt:T], [Xzero, Xtrue], 'm-'), hold on
+
+R = 4;
+Dt = R*dt;  % step size of EM
+L = N/R;    % L EM steps of size Dt = R*dt
+Xem = zeros(1,L); % preallocate for efficiency
+Xtemp = Xzero;
+
+for j = 1:L
+    Winc = sum(dW(R*(j-1) + 1:R*j));    % W increment in EM
+    Xtemp = Xtemp + Dt*lambda*Xtemp + mu*Xtemp*Winc;
+    Xem(j) = Xtemp;
+end
+
+plot([0:Dt:T], [Xzero, Xem],  'r--*'), hold off
+xlabel('t', 'FontSize', 12)
+ylabel('X', 'FontSize', 16, 'Rotation', 0, 'HorizontalAlignment', 'right')
+legend('exact solution', 'numerical solution', 'FontSize', 16)
+
+% Error at the end point
+emerr = abs(Xem(end) - Xtrue(end))   % this will decrease fior smaller R value
 
